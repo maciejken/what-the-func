25430e1bac04361df8fce485ab91bd311b19c1ee
Maciej Kędra <maciejken@gmail.com>
2025-10-01 22:31:05 +0200

    add more types and test cases

--- source/index.ts
+++ source/index.ts
@@ -2,8 +2,14 @@ import * as R from 'ramda';
 
 export const greet = R.replace('{name}', R.__, "Hello, {name}!");
 
-type color = -1 | 1;
-type move = [number, number, color];
-type value = 0 | color;
+export const empty = 0;
+export const black = -1;
+export const white = 1;
+
+export type color = typeof black | typeof white;
+export type move = [number, number, color];
+// let pass: move - with coordinates outside the board
+export type value = typeof empty | color;
+export type board = value[][];
 
 export const validate = ([x, y, color]: move, board: value[][]): boolean => true;
--- source/validate.test.ts
+++ source/validate.test.ts
@@ -1,29 +1,75 @@
-const { validate } = jest.requireActual('.');
+import { black as x, white as o, empty as _, board, validate } from ".";
 
 describe('validate', () => {
-  it('returns true if move is valid', () => {
-    const move = [2, 2, -1];
-    const board = [
-      [0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0],
+  it('returns true if stone is put on empty board', () => {
+    const b: board = [
+      [_, _, _, _, _],
+      [_, _, _, _, _],
+      [_, _, _, _, _],
+      [_, _, _, _, _],
+      [_, _, _, _, _],
     ];
 
-    expect(validate(move, board)).toBe(true);
+    expect(validate([2, 2, x], b)).toBe(true);
   });
 
-    it('returns false if move is invalid', () => {
-    const move = [2, 2, -1];
-    const board = [
-      [0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0],
-      [0, 0, 1, 0, 0],
-      [0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0],
+  it('returns false if stone is put on non-empty point', () => {
+    const b: board = [
+      [_, _, _, _, _],
+      [_, _, _, _, _],
+      [_, _, o, _, _],
+      [_, _, _, _, _],
+      [_, _, _, _, _],
     ];
 
-    expect(validate(move, board)).toBe(false);
+    expect(validate([2, 2, x], b)).toBe(false);
+  });
+
+  it('returns true if black stone is put on empty point next to point occupied by white', () => {
+    const b: board = [
+      [_, _, _, _, _],
+      [_, _, _, _, _],
+      [_, _, o, _, _],
+      [_, _, _, _, _],
+      [_, _, _, _, _],
+    ];
+
+    expect(validate([3, 2, x], b)).toBe(true);
+  });
+
+  it('returns false if black stone is put on single empty point surrounded by white stones', () => {
+    const b: board = [
+      [_, _, _, _, _],
+      [_, _, o, _, _],
+      [_, o, _, o, _],
+      [_, _, o, _, _],
+      [_, _, _, _, _],
+    ];
+
+    expect(validate([2, 2, x], b)).toBe(false);
+  });
+
+  it('returns true if black passes', () => {
+    const b: board = [
+      [_, _, _, _, _],
+      [_, _, o, _, _],
+      [_, _, _, _, _],
+      [_, _, _, _, _],
+      [_, _, _, _, _],
+    ];
+
+    expect(validate([-1, -1, x], b)).toBe(true);
+  });
+
+  it('returns true if white passes', () => {
+    const b: board = [
+      [_, _, _, _, _],
+      [_, _, _, _, _],
+      [_, _, _, o, _],
+      [_, _, x, _, _],
+      [_, _, _, _, _],
+    ];
+
+    expect(validate([-1, -1, o], b)).toBe(true);
   });
 })
